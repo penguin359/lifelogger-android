325529563c2f1929d7ee697f41ee14094291c8ae
Loren M. Lang <lorenl@north-winds.org>
2011-08-07 01:22:15 -0400

    Fixed a small problem with a previous merge to prevent a problem with a future merge.

--- src/org/northwinds/photocatalog/Main.java
+++ src/org/northwinds/photocatalog/Main.java
@@ -213,7 +213,7 @@ public class Main extends Activity {
 				mService = null;
 			}
 		}
-		////@Override
+		//@Override
 		public void onServiceDisconnected(ComponentName className) {
 			mService = null;
 		}
