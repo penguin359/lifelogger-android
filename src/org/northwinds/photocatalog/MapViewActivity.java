177318834ba5eaf163d21d701321cafbb9ba9b7c
Loren M. Lang <lorenl@north-winds.org>
2011-08-08 12:34:11 -0400

    Added some test Overlay markers on the MapView.

--- src/org/northwinds/photocatalog/MapViewActivity.java
+++ src/org/northwinds/photocatalog/MapViewActivity.java
@@ -28,18 +28,63 @@
 
 package org.northwinds.photocatalog;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.AlertDialog;
 //import android.app.Activity;
 //import android.content.Intent;
 //import android.content.ServiceConnection;
 //import android.content.SharedPreferences;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 //import android.preference.PreferenceManager;
 //import android.widget.Toast;
 
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class MapViewActivity extends MapActivity {
+	private static class MapItemizedOverlay extends ItemizedOverlay<OverlayItem> {
+		private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+		private Context mContext;
+
+		public MapItemizedOverlay(Drawable defaultMarker, Context context) {
+			super(boundCenterBottom(defaultMarker));
+			mContext = context;
+		}
+
+		public void addOverlay(OverlayItem overlay) {
+			mOverlays.add(overlay);
+			populate();
+		}
+
+		@Override
+		protected OverlayItem createItem(int i) {
+			return mOverlays.get(i);
+		}
+
+		@Override
+		public int size() {
+			return mOverlays.size();
+		}
+		
+		@Override
+		protected boolean onTap(int index) {
+			OverlayItem item = mOverlays.get(index);
+			AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+			dialog.setTitle(item.getTitle());
+			dialog.setMessage(item.getSnippet());
+			dialog.show();
+			return true;
+		}
+	}
+
 	@Override
 	protected boolean isRouteDisplayed() {
 		return false;
@@ -51,6 +96,16 @@ public class MapViewActivity extends MapActivity {
 		setContentView(R.layout.map_view);
 		MapView mapView = (MapView)findViewById(R.id.map_view);
 		mapView.setBuiltInZoomControls(true);
+		List<Overlay> mapOverlays = mapView.getOverlays();
+		Drawable drawable = getResources().getDrawable(R.drawable.androidmarker);
+		MapItemizedOverlay itemizedOverlay = new MapItemizedOverlay(drawable, this);
+		GeoPoint point = new GeoPoint(19240000,-99120000);
+		OverlayItem overlayItem = new OverlayItem(point, "Hola, Mundo!", "I'm in Mexico City!");
+		itemizedOverlay.addOverlay(overlayItem);
+		GeoPoint point2 = new GeoPoint(35410000, 139460000);
+		OverlayItem overlayItem2 = new OverlayItem(point2, "Sekai, konichiwa!", "I'm in Japan!");
+		itemizedOverlay.addOverlay(overlayItem2);
+		mapOverlays.add(itemizedOverlay);
 	}
 
 	//@Override
