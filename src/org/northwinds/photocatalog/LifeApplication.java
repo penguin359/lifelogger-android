5e2804fc020311996cc6c9bdd3ea9c7ea9aa2d87
Loren M. Lang <lorenl@north-winds.org>
2011-11-07 18:40:59 -0800

    Added a toast notification when crash reports are sent by ACRA.

--- src/org/northwinds/photocatalog/LifeApplication.java
+++ src/org/northwinds/photocatalog/LifeApplication.java
@@ -3,9 +3,13 @@ package org.northwinds.photocatalog;
 import android.app.Application;
 
 import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
-@ReportsCrashes(formKey = "dGExM01WVDVDSG5Ja1c0aXY1Wm9qTUE6MQ")
+@ReportsCrashes(formKey = "dGExM01WVDVDSG5Ja1c0aXY1Wm9qTUE6MQ",
+		mode = ReportingInteractionMode.TOAST,
+		forceCloseDialogAfterToast = false,
+		resToastText = R.string.crash_toast_text)
 public class LifeApplication extends Application {
 	@Override
 	public void onCreate() {
