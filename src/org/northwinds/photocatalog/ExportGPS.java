fbf8afa93e810cfd950f0984346e164a9ab1b08f
Loren M. Lang <lorenl@north-winds.org>
2013-07-30 06:40:58 -0700

    Added build scripts for Ant, and an initial test project.

--- src/org/northwinds/photocatalog/ExportGPS.java
+++ src/org/northwinds/photocatalog/ExportGPS.java
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (c) 2010-2011, Loren M. Lang
  * All rights reserved.
  *
---
+++ tests/src/org/northwinds/photocatalog/tests/MainActivityTest.java
@@ -0,0 +1,72 @@
+package org.northwinds.photocatalog.tests;
+
+import org.northwinds.photocatalog.MainActivity;
+
+import android.app.Activity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.KeyEvent;
+import android.widget.Button;
+
+public class MainActivityTest extends
+		ActivityInstrumentationTestCase2<MainActivity> {
+	private Button mStartButton;
+	private Activity mActivity;
+	
+	public MainActivityTest() {
+		super("org.northwinds.photocatalog", MainActivity.class);
+	}
+	 /*
+     * Sets up the test environment before each test.
+     * @see android.test.ActivityInstrumentationTestCase2#setUp()
+     */
+    @Override
+    protected void setUp() throws Exception {
+
+        /*
+         * Call the super constructor (required by JUnit)
+         */
+
+	super.setUp();
+
+    /*
+     * prepare to send key events to the app under test by turning off touch mode.
+     * Must be done before the first call to getActivity()
+     */
+
+    setActivityInitialTouchMode(false);
+
+    /*
+     * Start the app under test by starting its main activity. The test runner already knows
+     * which activity this is from the call to the super constructor, as mentioned
+     * previously. The tests can now use instrumentation to directly access the main
+     * activity through mActivity.
+     */
+    mActivity = getActivity();
+    mStartButton = (Button)mActivity.findViewById(org.northwinds.photocatalog.R.id.start_but);
+    }
+    public void testPreconditions() {
+    	assertTrue(true);
+    }
+    public void testHaveFun() {
+        mActivity.runOnUiThread(
+                new Runnable() {
+                    public void run() {
+                        mStartButton.requestFocus();
+                        
+                    }
+                }
+            );
+        this.sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
+        assertEquals(true, false);
+    }
+	public void testStateDestroy() {
+		// Halt the Activity by calling Activity.finish() on it
+
+		mActivity.finish();
+
+    	// Restart the activity by calling ActivityInstrumentationTestCase2.getActivity()
+
+    	mActivity = this.getActivity();
+	}
+
+}
